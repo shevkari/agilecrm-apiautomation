b5085582470a50e783a681764c1fc1b3148e40ca
shevkari <shevkariamol19@gmail.com>
2024-01-15 12:43:12 +0530

    removed unwaded spaces

--- src/main/java/com/agilecrm_automation/common/PropertyHandler.java
+++ src/main/java/com/agilecrm_automation/common/PropertyHandler.java
@@ -14,10 +14,8 @@ public class PropertyHandler {
         String filepath = System.getProperty("user.dir")+"/src/test/resources/properties/"+path;
         File file = new File(filepath);
         FileReader reader = new FileReader(file);
-
         Properties properties = new Properties();
         properties.load(reader);
-
        return properties.getProperty(key);
 
 
